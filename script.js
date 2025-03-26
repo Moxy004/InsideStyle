5c5b78c96d11c8aeb2aded02ebebbf7e90a88768
Jaren <lunajaren924@gmail.com>
2025-03-26 15:54:57 +0800

    Responsiveness

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const hNav = document.querySelector(".header_nav");
+const backDrop = document.querySelector(".backDrop");
+toggleMenu.addEventListener("click", () =>{
+    toggleMenu.classList.toggle("open");
+    hNav.classList.toggle("open");
+    backDrop.classList.toggle("open");
+});
