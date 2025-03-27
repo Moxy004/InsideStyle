a58f9f4ddc797bbb4336963661411c9bfebf459b
Jaren <lunajaren924@gmail.com>
2025-03-27 12:13:38 +0800

    4 Page Website

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const hNav = document.querySelector(".header_nav");
 const backDrop = document.querySelector(".backDrop");
+
+
 toggleMenu.addEventListener("click", () =>{
     toggleMenu.classList.toggle("open");
     hNav.classList.toggle("open");
     backDrop.classList.toggle("open");
+    console.log("Button is clicked");
 });
